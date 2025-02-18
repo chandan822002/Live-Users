6fd2d6ffe1847332d6a836933d8795b6458314d0
Jatoliyabiju <jatoliyabiju1611@gmail.com>
2025-02-18 15:35:23 +0530

    Initial commit

--- index.js
+++ index.js
@@ -73,5 +73,5 @@ app.get("/api/users", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
